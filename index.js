75741e9252dff695e56867e0755a1079b765c3b4
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-26 16:41:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ class Driver {
     return (new Date(year)) - (yearDate.prototype.getFullYear(yearstartDate()))
   }
 }
+
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
