4c068e9d3a497083caf42cef1196c6b896b92e2c
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-26 16:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ class Route {
     this.endingLocation = endingLocation;
   }
   
-  blocksTravelled() {
+  blocksTravelled(beginningLocation, endingLocation) {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     
   }
 }
