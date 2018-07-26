227565fd4ebd4dd3f4f822871e7f1f7eb5477740
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-26 16:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,10 @@ class Route {
   blocksTravelled(beginningLocation, endingLocation) {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     
+    
+  }
+  
+  estimatedTime(peak) {
+    
   }
 }
